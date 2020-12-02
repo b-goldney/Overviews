85af3566c33936d1801d5a95058b9ea197785c14
b-goldney <BrandonGoldney@gmail.com>
2020-12-02 17:18:59 -0600

    Updated ellipsis.cpp

--- c++ test code/ellipsis.cpp
+++ c++ test code/ellipsis.cpp
@@ -1,11 +1,18 @@
+//Purpose:  demonstrate how we can pass a variable number of arguments to a
+//function
+
+// Example adapted from: https://www.cprogramming.com/tutorial/c/lesson17.html
+
 #include <iostream>
 
 using std::cout;
 using std::endl;
 
-int sum(int n,...) // ... is for a variable number of arguments
+int sum(int n,...) // "..." is for a variable number of arguments, the n says how many arguments will be given
 {
-	va_list list; // we need a va_list
+	va_list list; // we need a va_list to hold all the arguments to be summed
+
+    //va_start "Initializes a variable argument list"
 	va_start(list, n); // the va_list, named as "list", takes n arguments
 
 	int x;
@@ -16,12 +23,14 @@ int sum(int n,...) // ... is for a variable number of arguments
 		s+=x;
 	}
 
+    va_end(list);
+
 	return s;
 }
 
 int main()
 { 
 	cout << sum(3,10,20,30) << endl;
-	cout << sum(1,2,3) << endl; //Notice we can put in a different # of arguments
+	cout << sum(2,2,3) << endl; //Notice we can put in a different # of arguments
 }
 
